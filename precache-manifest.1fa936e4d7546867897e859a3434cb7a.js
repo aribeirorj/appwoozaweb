b25cef367ce505a7c89e82762f4a2bb1a0ba6a24
André Luiz Ribeiro da Silva <aribeiro.rj@gmail.com>
2019-10-04 19:21:50 -0300

    Updates

---
+++ precache-manifest.1fa936e4d7546867897e859a3434cb7a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e036d99f340955b23d908129a1c5ae6",
+    "url": "/appwoozaweb/index.html"
+  },
+  {
+    "revision": "151083c8aa9e6f2e67bb",
+    "url": "/appwoozaweb/static/js/2.ec6a9d54.chunk.js"
+  },
+  {
+    "revision": "67a0c6e9df69fc89e9c1",
+    "url": "/appwoozaweb/static/js/main.8fffd2ff.chunk.js"
+  },
+  {
+    "revision": "532efd036fbb04bd0ae5",
+    "url": "/appwoozaweb/static/js/runtime-main.9a7f5a1d.js"
+  }
+]);
