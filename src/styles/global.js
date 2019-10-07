6bfc8a5a294ff8ea2f887d24df4e94067e42b326
André Luiz Ribeiro da Silva <aribeiro.rj@gmail.com>
2019-10-07 20:36:19 -0300

    aplicando media query, para adaptar a aplicacao para mobile

--- src/styles/global.js
+++ src/styles/global.js
@@ -1,4 +1,5 @@
 import { createGlobalStyle } from 'styled-components';
+import { device } from './device';
 
 const GlobalStyle = createGlobalStyle`
   *{
@@ -16,6 +17,14 @@ const GlobalStyle = createGlobalStyle`
     background:#3B3B43;
     font-size: 14px;
     font-family: Arial, Helvetica, sans-serif
+  }
+
+  @media ${device.laptop} {
+    max-width: 800px;
+  }
+
+  @media ${device.desktop} {
+    max-width: 1400px;
   }
 
  `;
